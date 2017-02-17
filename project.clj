74cba31ddd5e4271ae30353b9856463637166d05
Octavian Geagla <ogeagla@gmail.com>
2017-02-17 11:43:43 -0700

    [deps] update dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojureapp "0.1.0-SNAPSHOT"
-    :plugins [[lein-ring "0.7.5"]]
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                   [http-kit "2.1.12"]]
+    :plugins [[lein-ring "0.11.0"]]
+    :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                   [http-kit "2.2.0"]]
     :local-repo ~(System/getenv "M2_REPO")
     :main webapp)
