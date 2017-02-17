0ad1b70af44bd357e769911a38144dc0fb9de0ba
Template builder <builder@example.com>
2017-02-17 10:53:55 -0500

    Creating template

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clojureapp "0.1.0-SNAPSHOT"
+    :plugins [[lein-ring "0.7.5"]]
+    :dependencies [[org.clojure/clojure "1.5.1"]
+                   [http-kit "2.1.12"]]
+    :local-repo ~(System/getenv "M2_REPO")
+    :main webapp)
