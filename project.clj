e44ceeafcb368377d8883e32943ddaa3c5cf5b66
Octavian Geagla <ogeagla@gmail.com>
2017-02-19 09:14:10 -0700

    [lein][git]

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
     :plugins [[lein-ring "0.11.0"]]
     :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                    [http-kit "2.2.0"]]
-    :local-repo ~(System/getenv "M2_REPO")
+    ;:local-repo ~(System/getenv "M2_REPO")
     :main webapp)
--- src/webapp.clj
+++ src/webapp.clj
@@ -9,4 +9,5 @@
 (defn -main [& args]
   (let [port (Integer/parseInt (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_PORT" "8080"))
         ip (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_IP" "0.0.0.0")]
+    (println "Running app on: " ip ":" port)
     (run-server app {:ip ip :port port})))
